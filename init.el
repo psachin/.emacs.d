b605b5a69d7b5066f21330ebe9b2f9c907c9e58d
Sachin Patil <iclcoolster@gmail.com>
2016-05-29 18:26:54 +0530

    Lets cleanup Emacs
    Signed-off-by: Sachin Patil <iclcoolster@gmail.com>

--- init.el
+++ init.el
@@ -1,41 +1,36 @@
+;;; Let the magic begin..
+
+;; Increase gc to 500MB for easy startup
+(setq gc-cons-threshold (* 500 1024 1024))
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
-(load-file "~/.emacs.d/general_config.el")
-(load-file "~/.emacs.d/install-packages.el")
-(load-file "~/.emacs.d/defalias.el")
-(load-file "~/.emacs.d/kbd_macro.el")
-(load-file "~/.emacs.d/package_init.el")
+;; Make sure Org is installed
+(unless (package-installed-p 'org)
+    (package-refresh-contents)
+    (package-install 'org))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ac-ispell-fuzzy-limit 4)
- '(ac-ispell-requires 4)
- '(custom-enabled-themes (quote (tango-dark)))
- '(insert-shebang-file-types
-   (quote
-    (("py" . "python")
-     ("sh" . "bash")
-     ("pl" . "perl")
-     ("rb" . "ruby"))))
- '(org-latex-minted-langs
-   (quote
-    ((emacs-lisp "common-lisp")
-     (cc "c++")
-     (cperl "perl")
-     (shell-script "bash")
-     (caml "ocaml")
-     (ruby "ruby"))))
- '(package-selected-packages
-   (quote
-    (yaml-mode iedit elpy git-timemachine find-file-in-project robe markdown-mode jedi yasnippet ac-ispell insert-shebang magit company autopair)))
- '(read-buffer-completion-ignore-case t)
- '(read-file-name-completion-ignore-case t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; From use-package Readme
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
+;; Load config.org - my Emacs configuration
+(org-babel-load-file (concat user-emacs-directory "ReadMe.org"))
+
+;; gc - decrease threshold to 5 MB
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+
+;;; init.el ends here
