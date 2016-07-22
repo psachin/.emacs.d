9aac4703e5eaebff90e53100af10f0a4a09af5d9
Sachin Patil <psachin@redhat.com>
2016-07-22 11:58:54 +0530

    Reveal.js
    - Added reveal.js(ox-reveal) package
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- init.el
+++ init.el
@@ -5,11 +5,13 @@
 
 (require 'package)
 (add-to-list 'package-archives
-	     '("gnu" . "http://elpa.gnu.org/packages/"))
+			 '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
+			 '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+			 '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 ;; Make sure Org is installed
